d4a96b6c9d60372f3e0b8bb33a2b28be3ecf9200
Thiago Machado da Silva <thy@hotmail.com.br>
2013-03-21 17:11:55 -0300

    test

--- 4040 - Unifei-Itabira Treino-7 (Equipe)/7 - 12531 - Hours and Minutes/main.cpp
+++ 4040 - Unifei-Itabira Treino-7 (Equipe)/7 - 12531 - Hours and Minutes/main.cpp
@@ -5,19 +5,25 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
 	
-	int a; cin >> a;
+	int a;
 	int i = -1, m, h, s;
 
-	while(++i < 720) {
-		m = i % 60; 
-		h = i / 12;
-		s = m - h;
-		if (s < 0) s *= -1;
-		if (s * 6 == a) {
-			cout << "Y\n";
-			break;
-		} 
+	while (cin >> a){
+
+		while(++i < 720) {
+			m = i % 60; 
+			h = i / 12;
+			s = m - h;
+			if (s < 0) s *= -1;
+			if (s * 6 == a) {
+				cout << "Y\n";
+				break;
+			} 
+		}
+		if (i == 720) cout << "N\n";
+		
 	}
-	if (i == 720) cout << "N\n";
+
+
 	return 0;
 }
