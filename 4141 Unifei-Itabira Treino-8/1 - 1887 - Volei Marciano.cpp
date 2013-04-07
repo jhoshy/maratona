01c8d1e6df137900ccde1d3498d6671614db5d67
Thiago Machado da Silva <thy@hotmail.com.br>
2013-04-07 04:48:49 -0300

    1 init

--- 4141 Unifei-Itabira Treino-8/1 - 1887 - Volei Marciano.cpp
+++ 4141 Unifei-Itabira Treino-8/1 - 1887 - Volei Marciano.cpp
@@ -1,2 +1,50 @@
 // http://br.spoj.com/problems/VOLEI/
 
+int main() {
+
+	while(1) {
+		int N;
+		cin >> N;
+		if (N == 0) return 0;
+
+		// monta estrutura da matrix
+		bool **matrix;
+
+		// matrix do input
+		{
+			int N2 = N, // N original
+				Xo = 0, Yo = 0, // offset 
+				**list = new int*[N];
+
+			// joga input numa lista
+			while(N--){ 
+				int X, Y;
+				cin >> X >> Y;
+				if (X < Xo) Xo = X;
+				if (Y < Yo) Yo = Y;
+				list[N] = new int[2];
+				list[N][0] = X;
+				list[N][1] = Y;
+			}
+
+			N = N2;
+			// monta estrutura da matrix
+			matrix = new bool*[N];
+			while(N--) {
+				matrix[N] = new bool[N];
+				int M = N2;
+				while(M--) matrix[N][M] = false;
+			}
+
+			N = N2;
+			// monta matrix, tudo positivo
+			while(N--) matrix[list[N][1] + Yo][list[N][0] + Xo] = true;
+		}
+
+
+
+
+	}
+
+	return 0;
+}
